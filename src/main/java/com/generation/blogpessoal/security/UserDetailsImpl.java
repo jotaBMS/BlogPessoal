dacb4cad458817ee8260a2af5c4558419c0f6ab5
João Bispo <joaovitor.bispo.1000@gmail.com>
2024-12-17 22:47:45 -0300

    erro de login corrigido

--- src/main/java/com/generation/blogpessoal/security/UserDetailsImpl.java
+++ src/main/java/com/generation/blogpessoal/security/UserDetailsImpl.java
@@ -26,19 +26,19 @@ public class UserDetailsImpl implements UserDetails {
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
-		return null;
+		return authorities;
 	}
 
 	@Override
 	public String getPassword() {
 		// TODO Auto-generated method stub
-		return null;
+		return password;
 	}
 
 	@Override
 	public String getUsername() {
 		// TODO Auto-generated method stub
-		return null;
+		return userName;
 	}
 	
 	@Override
